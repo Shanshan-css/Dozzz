8c014b8f407b131b9f38467eb9aec2118bc00cd9
Shanshan-css <49906256+Shanshan-css@users.noreply.github.com>
2023-09-25 22:44:18 +0200

    Add files via upload

---
+++ App for dutch/java/com/example/snoozy22/ui/MessageAdapter.java
@@ -0,0 +1,15 @@
+package com.example.snoozy22.ui;
+
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MessageAdapter {
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
+    }
+}
